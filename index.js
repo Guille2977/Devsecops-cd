fe82921531c6ac4bc1bd280776558822af488847
Guillermo Uribe <guilleum390@gmail.com>
2026-06-10 21:22:27 -0500

    Actualiza version para evidenciar CD

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send(`
     <h1>Aplicación DevSecOps - CD</h1>
-    <p>Desplegada automáticamente en Azure App Service.</p>
-    <p>Versión 1.0</p>
+    <p>Desplegada automáticamente en Render mediante GitHub.</p>
+    <p>Versión 2.0 - Cambio desplegado automáticamente con CD</p>
   `);
 });
 
